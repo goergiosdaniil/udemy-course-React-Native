5cc706cd9585a4a1c15f9944971bbe5172648f17
goergiosdaniil <goergios2@gmail.com>
2024-02-19 12:09:17 +0200

    Some additions

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js A gamisσ to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer} >
+        <TextInput style={styles.textInput} placeholder="Your goal" />
+        <Button title="ADD" />
+      </View>
+      <View style={styles.goalsContainer} >
+        <Text>Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+  },
+  textInput: {
+    width: "70%",
+    borderColor: "black",
+    borderWidth: 1,
+    marginRight: 8,
+    padding: 10,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
--- README.md
+++ README.md
@@ -5,3 +5,11 @@ This folder is dedicated to the React Native course on Udemy.
 Please note that all the code and resources related to the course will be stored in this folder. Make sure to keep it organized and follow the instructions provided in the course materials.
 
 Happy learning!
+
+16/02/2024
+
+I finished Section 1
+Into section 2, we will create a course goals app
+
+19/02/2024
+Finished 22 Video of Section 2
